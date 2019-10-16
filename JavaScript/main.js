08ebe4a9d595bc34670e760e21a8079afb505053
xuziang111 <584788096@qq.com>
2019-10-16 17:31:43 +0800

    ajac

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -28,48 +28,51 @@ let xxx= new Vue({
     el:'#body-container',
     router,
     data:{
-        type:[{type:'热血',href:'#'},
-            {type:'恋爱',href:'#'},
-            {type:'百合',href:'#'},
-            {type:'彩虹',href:'#'},
-            {type:'冒险',href:'#'},
-            {type:'后宫',href:'#'},
-            {type:'悬疑',href:'#'},
-            {type:'推理',href:'#'},
-            {type:'搞笑',href:'#'},
-            {type:'奇幻',href:'#'},
-            {type:'萌系',href:'#'},
-            {type:'日常',href:'#'},
-            {type:'治愈',href:'#'},
-            {type:'穿越',href:'#'},
-        ],
-        list:[{name:'地区',type:[{type:'全部',href:'#',classname:''},{type:'日漫',href:'#',classname:''},{type:'国漫',href:'#',classname:''},{type:'美漫',href:'#',classname:''}]},
-            {name:'进度',type:[{type:'全部',href:'#',classname:''},{type:'连载',href:'#',classname:''},{type:'完结',href:'#',classname:''}]},
-            {name:'类别',type:[{type:'热血',href:'#'},
-            {type:'恋爱',href:'#'},
-            {type:'百合',href:'#'},
-            {type:'彩虹',href:'#'},
-            {type:'冒险',href:'#'},
-            {type:'后宫',href:'#'},
-            {type:'悬疑',href:'#'},
-            {type:'推理',href:'#'},
-            {type:'搞笑',href:'#'},
-            {type:'奇幻',href:'#'},
-            {type:'萌系',href:'#'},
-            {type:'日常',href:'#'},
-            {type:'治愈',href:'#'},
-            {type:'穿越',href:'#'},]}
-        ],
+        // type:[{type:'热血',href:'#'},
+        //     {type:'恋爱',href:'#'},
+        //     {type:'百合',href:'#'},
+        //     {type:'彩虹',href:'#'},
+        //     {type:'冒险',href:'#'},
+        //     {type:'后宫',href:'#'},
+        //     {type:'悬疑',href:'#'},
+        //     {type:'推理',href:'#'},
+        //     {type:'搞笑',href:'#'},
+        //     {type:'奇幻',href:'#'},
+        //     {type:'萌系',href:'#'},
+        //     {type:'日常',href:'#'},
+        //     {type:'治愈',href:'#'},
+        //     {type:'穿越',href:'#'},
+        // ],
+        // list:[{name:'地区',type:[{type:'全部',href:'#',classname:''},{type:'日漫',href:'#',classname:''},{type:'国漫',href:'#',classname:''},{type:'美漫',href:'#',classname:''}]},
+        //     {name:'进度',type:[{type:'全部',href:'#',classname:''},{type:'连载',href:'#',classname:''},{type:'完结',href:'#',classname:''}]},
+        //     {name:'类别',type:[{type:'热血',href:'#'},
+        //     {type:'恋爱',href:'#'},
+        //     {type:'百合',href:'#'},
+        //     {type:'彩虹',href:'#'},
+        //     {type:'冒险',href:'#'},
+        //     {type:'后宫',href:'#'},
+        //     {type:'悬疑',href:'#'},
+        //     {type:'推理',href:'#'},
+        //     {type:'搞笑',href:'#'},
+        //     {type:'奇幻',href:'#'},
+        //     {type:'萌系',href:'#'},
+        //     {type:'日常',href:'#'},
+        //     {type:'治愈',href:'#'},
+        //     {type:'穿越',href:'#'},]}
+        // ],
     },
     methods:{
-        startsearch:function(e){
-            console.log(e.currentTarget)
-            let temp1 = e.currentTarget.getAttribute('data-index1')
-            let temp2 = e.currentTarget.getAttribute('data-index2')
-            for(let j = 0;j<this.list[temp1].type.length;j++){
-                this.list[temp1].type[j].classname = ''
-            }
-            this.list[temp1].type[temp2].classname='active'
+        // startsearch:function(e){
+        //     console.log(e.currentTarget)
+        //     let temp1 = e.currentTarget.getAttribute('data-index1')
+        //     let temp2 = e.currentTarget.getAttribute('data-index2')
+        //     for(let j = 0;j<this.list[temp1].type.length;j++){
+        //         this.list[temp1].type[j].classname = ''
+        //     }
+        //     this.list[temp1].type[temp2].classname='active'
+
+        // }
+        searchmanhua:function(manhua){
 
         }
     },
--- JavaScript/topbar.js
+++ JavaScript/topbar.js
@@ -17,53 +17,57 @@ let topbar = Vue.component('topbar',{
     </ul>
 </div>
     `,
-
+//type area progress word
     data:function(){
         return {
-            type:[{type:'热血',href:'#'},
-            {type:'恋爱',href:'#'},
-            {type:'百合',href:'#'},
-            {type:'彩虹',href:'#'},
-            {type:'冒险',href:'#'},
-            {type:'后宫',href:'#'},
-            {type:'悬疑',href:'#'},
-            {type:'推理',href:'#'},
-            {type:'搞笑',href:'#'},
-            {type:'奇幻',href:'#'},
-            {type:'萌系',href:'#'},
-            {type:'日常',href:'#'},
-            {type:'治愈',href:'#'},
-            {type:'穿越',href:'#'},
+            type:[{type:'热血',href:'#',classname:'active'},
+            {type:'恋爱',href:'#',classname:''},
+            {type:'百合',href:'#',classname:''},
+            {type:'彩虹',href:'#',classname:''},
+            {type:'冒险',href:'#',classname:''},
+            {type:'后宫',href:'#',classname:''},
+            {type:'悬疑',href:'#',classname:''},
+            {type:'推理',href:'#',classname:''},
+            {type:'搞笑',href:'#',classname:''},
+            {type:'奇幻',href:'#',classname:''},
+            {type:'萌系',href:'#',classname:''},
+            {type:'日常',href:'#',classname:''},
+            {type:'治愈',href:'#',classname:''},
+            {type:'穿越',href:'#',classname:''},
         ],
-        list:[{name:'地区',type:[{type:'全部',href:'#',classname:''},{type:'日漫',href:'#',classname:''},{type:'国漫',href:'#',classname:''},{type:'美漫',href:'#',classname:''}]},
-            {name:'进度',type:[{type:'全部',href:'#',classname:''},{type:'连载',href:'#',classname:''},{type:'完结',href:'#',classname:''}]},
-            {name:'字母',type:[{type:'热血',href:'#'},
-            {type:'恋爱',href:'#'},
-            {type:'百合',href:'#'},
-            {type:'彩虹',href:'#'},
-            {type:'冒险',href:'#'},
-            {type:'后宫',href:'#'},
-            {type:'悬疑',href:'#'},
-            {type:'推理',href:'#'},
-            {type:'搞笑',href:'#'},
-            {type:'奇幻',href:'#'},
-            {type:'萌系',href:'#'},
-            {type:'日常',href:'#'},
-            {type:'治愈',href:'#'},
-            {type:'穿越',href:'#'},]}
+        list:[{par:'area',name:'地区',type:[{type:'全部',href:'#',classname:'active'},{type:'日漫',href:'#',classname:''},{type:'国漫',href:'#',classname:''},{type:'美漫',href:'#',classname:''}]},
+            {par:'progress',name:'进度',type:[{type:'全部',href:'#',classname:'active'},{type:'连载',href:'#',classname:''},{type:'完结',href:'#',classname:''}]},
+            {par:'word',name:'字母',type:[{type:'热血',classname:'active'},{type:'恋爱',classname:''},
+            {type:'百合',classname:''},]}
         ],
         }
     },
     methods:{
         startsearch:function(e){
-            console.log(e.currentTarget)
             let temp1 = e.currentTarget.getAttribute('data-index1')
             let temp2 = e.currentTarget.getAttribute('data-index2')
             for(let j = 0;j<this.list[temp1].type.length;j++){
                 this.list[temp1].type[j].classname = ''
             }
             this.list[temp1].type[temp2].classname='active'
-
+            let temp ={}
+            for(let i=0;i<this.type.length;i++){
+                if(this.type[i].classname=='active'){
+                    temp.type = this.type[i].type
+                    break
+                }
+            }
+            for(let i=0;i<this.list.length;i++){
+                for(let j=0;j<this.list[i].type.length;i++){
+                    if(this.list[i].type[j].classname=='active'){
+                        temp[this.list[i].par]= this.list[i].type[j].type
+                        break
+                    }
+                }
+            }
+            console.log(temp)
+            // debugger
+            this.$emit('searchmanhua',temp)
         }
     },
 })
